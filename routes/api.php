8322ed455f64754d05f2ba06c644b5c8052d5021
Nadlar Aditya <nadlaraditya13@gmail.com>
2026-04-04 23:24:58 +0700

    feat: add api routes

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
     return response()->json(['status' => 'ok']);
+    Route::post('/cart', [CartController::class, 'add']);
+    Route::get('/cart', [CartController::class, 'index']);
+
+    Route::get('/products', [ProductController::class, 'index']);
+
+    Route::post('/checkout', [OrderController::class, 'checkout']);
 });
